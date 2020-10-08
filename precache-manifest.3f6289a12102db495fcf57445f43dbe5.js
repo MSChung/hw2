4e36671da714d115291e91808e3132b2709f0237
MSChung <b06901108@ntu.edu.tw>
2020-10-08 17:05:37 +0800

    Updates

---
+++ precache-manifest.3f6289a12102db495fcf57445f43dbe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f483aabbd0f070a954b68069132048fb",
+    "url": "/hw2/index.html"
+  },
+  {
+    "revision": "36af98e1cacb032e5f7f",
+    "url": "/hw2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3746d030e89a8f3de959",
+    "url": "/hw2/static/js/2.4c0c2776.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw2/static/js/2.4c0c2776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36af98e1cacb032e5f7f",
+    "url": "/hw2/static/js/main.10475b98.chunk.js"
+  },
+  {
+    "revision": "93a6f746c1662466389b",
+    "url": "/hw2/static/js/runtime-main.97906763.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hw2/static/media/logo.5d5d9eef.svg"
+  }
+]);
